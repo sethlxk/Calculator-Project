68b8a99cb689d267e1e278ccee1df32be0bd7715
sethl <sethlxk@gmail.com>
2023-06-15 09:49:32 +0800

    final commit

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 const operators = ['/','x','-','+']
-let input = ''
-
+let input = '';
+let temp1;
+let operator;
 
 function selectValue(e){
     if(input.includes('.') && e.innerText === '.'){ //if there is already a decimal point included, cannot add anymore decimal
         return;
     }
-    if(!operators.includes(e.innerText)){ //if the button is not an operator
+    if(!operators.includes(e.innerText) && e.innerText != '='){ //if the button is not an operator and its not equal
         input += e.innerText;
+        displayValue(input);
     }
-    displayValue(input);
+    else if(operators.includes(e.innerText)){ //if the button is an operator, i want to store the current value and the operator sign
+        temp1 = input;
+        operator = e.innerText;
+        displayValue(input);
+        input = '';
+    }
+    else{   //if it is an equal button
+        operate(temp1,input,operator);
+    }
 }
 
 
@@ -18,6 +28,23 @@ function displayValue(e){
 }
 
 
-const operate = (x,y) => {
-    
+const operate = (x,y,z) => {
+    x = parseFloat(x);
+    y = parseFloat(y);
+    let result;
+    switch(z) {
+        case '+':
+            result = x+y;
+            break;
+        case '-':
+            result = x-y;
+            break;
+        case 'x':
+            result = x*y;
+            break;
+        case '/':
+            result = x/y;
+            break;
+    }
+    displayValue(result);
 }
